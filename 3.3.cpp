2585353f1e9b64705a1aabf639c68b39eb295dab
jhonalexcusi <135255944+jhonalexcusi@users.noreply.github.com>
2023-06-09 11:20:31 -0500

    Update 3.3.cpp

--- 3.3.cpp
+++ 3.3.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-    float a, b, c, d, e, f;
-
-    cout << "Ingrese los coeficientes a, b, c, d, e y f de la siguiente manera: ax + by = c, y\n";
-    cout << "cx + dy = f\n";
-    cin >> a >> b >> c >> d >> e >> f;
-
-    float det = a*d - b*c;  // calculamos la determinante del sistema
-
-    if (det == 0) {
-        cout << "El sistema no tiene solución única." << endl;
-    } else {
-        float x = (d*e - b*f) / det;  // calculamos la solución para x
-        float y = (a*f - c*e) / det;  // calculamos la solución para y
-        
-        cout << "x = " << x << ", y = " << y << endl;  // mostramos la solución del sistema
-    }
-
-    return 0;
+   float a, b, c, d, e, f;
+   float D, Dx, Dy, x, y; 
+   
+   cout << "Ingrese los coeficientes a, b, c, d, e, y f: ";
+   cin >> a >> b >> c >> d >> e >> f;
+   
+   D = a * d - b * c;
+   Dx = e * d - b * f;
+   Dy = a * f - e * c;
+   
+   x = Dx / D;
+   y = Dy / D;
+   
+   if (x > 2) {
+      y = pow(y, 2);
+   }
+   
+   cout << "La solución para x es: " << x << endl;
+   cout << "La solución para y es: " << y << endl;
+   
+   return 0;
 }
+
 // MODIFICAR EL EJERCICIO PARA EL CASO QUE SI EL RESULTADO DE "X" ES MAYOR QUE 2, EL RESULTADO DE "Y", DEBERA SER ELEVADO AL CUADRADO, CASO CONTRARIO SE LE RESTARA 2
